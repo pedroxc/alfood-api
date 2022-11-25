d866cc0f6422e02bdf8bedf64f30c42983d37c33
Pedro Coelho <pedroxaviercoelho@hmail.com>
2022-11-25 14:45:52 -0300

    first commit

--- setup/settings.py
+++ setup/settings.py
@@ -135,10 +135,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 MEDIA_URL = '/media/'
